1a42cdc6bf9b5b816d321452c7e8c20a58a6a55f
Lucas Gabriel Pereira de Menezes <111756599+LucasGPM97@users.noreply.github.com>
2023-08-26 00:26:40 -0300

    Questao 5 Concluida

--- Questao05.c
+++ Questao05.c
@@ -8,6 +8,7 @@ que permita ao usuário adicionar informações de livros, listar todos os livro
 //Lucas Gabriel Pereira de Menezes - UC22101832
 
 #include <stdio.h>
+#define TAM_LIVRO 10
 
 typedef struct {
     char titulo[50];
@@ -16,25 +17,62 @@ typedef struct {
 
 }Livro;
 
-void setLivro(Livro l[], char titulo[50], char autor[15], int* qtd_livros);  //Cadastro por passagem de parametros (Constructor)
-void cadastroContato(Livro l[], int* qtd_livros);                               //Cadastro por entrada de dados  
-void imprimeContato(Livro l);
+void setLivro(Livro l[], char titulo[50], char autor[15], int anoPublicacao, int* qtd_livros);  //Cadastro por passagem de parametros (Constructor)
+void cadastroLivro(Livro l[], int* qtd_livros);                               //Cadastro por entrada de dados 
+void imprimeLivro(Livro c[], int i);
+void listarLivro(Livro c[], int* qtd_livros);
+void procurarLivro(Livro c[], char nome[50], int* qtd_livros);
+
 
 int main(){
 
-    Livro l;
+    Livro l[TAM_LIVRO];
+    int escolha=0, qtd_livros=0;
+    char autor[50];
 
 
+    do{
+        system("cls");
+        printf("\n********** Menu **********\n");
+        printf("1 - Cadastrar novo Livro\n");
+        printf("2 - Listar todos os Livros\n");
+        printf("3 - Buscar um livro pelo autor\n");
+        printf("4 - Sair\n");
+
+        scanf("%d", &escolha);
 
 
+        switch (escolha)
+        {
+        case 1:
+            cadastroLivro(l, &qtd_livros);
+            qtd_livros++;
+            system("PAUSE");
+            break;
+        case 2:
+            listarLivro(l, &qtd_livros);
+            system("PAUSE");
+            break;
+        case 3:
+            printf("Digite o nome do autor: ");
+            scanf(" %[^\n]s", &autor);
+            procurarLivro(l, autor, &qtd_livros);
+            system("PAUSE");
+            break;
+        
+        default:
+            break;
+        }
+    }while(escolha != 4);
 
     return 0;
 }
 
 
-void setLivro(Livro l[], char titulo[50], char autor[15], int* qtd_livros){
+void setLivro(Livro l[], char titulo[50], char autor[15], int anoPublicacao, int* qtd_livros){
     strcpy(l[*qtd_livros].titulo,titulo);
     strcpy(l[*qtd_livros].autor,autor);
+    l[*qtd_livros].anoPublicacao = anoPublicacao;
 
 }
 
@@ -44,14 +82,32 @@ void cadastroLivro(Livro l[], int* qtd_livros){
     scanf(" %[^\n]s", &l[*qtd_livros].titulo);
     printf("Autor: ");
     scanf(" %[^\n]s", &l[*qtd_livros].autor);
+    printf("Ano publicacao: ");
+    scanf("%d", &l[*qtd_livros].anoPublicacao);
 
 }
 
 
-void imprimeLivro(Livro l){
+void imprimeLivro(Livro l[], int i){
 
-    printf("\n************************************************\n");
-    printf("titulo: %s\tautor: %s", l.titulo, l.autor);
-    printf("\n************************************************\n");
+    printf("titulo: %s\tautor: %s\tano publicacao: %d\n", l[i].titulo, l[i].autor, l[i].anoPublicacao);
+
+}
+
+
+void listarLivro(Livro l[], int* qtd_Livro){
+    int i=0;
+    for(i=0;i<*qtd_Livro;i++){
+        imprimeLivro(l, i);
+    }
+}
+
+void procurarLivro (Livro l[], char autor[50], int* qtd_livros ){
+    int i=0;
+    for(i=0;i<=*qtd_livros;i++){
+        if(strcmp(l[i].autor,autor) == 0){
+            imprimeLivro(l, i);
+        }
+    }
 
 }
